7931ab430c83f86e93bdf902485e2cd95c22a371
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2020-12-14 21:22:41 -0300

    create-an-unique-app

---
+++ scripts.js
@@ -0,0 +1,39 @@
+
+// const inicio = document.querySelector('#inicio')
+// const videos = document.querySelector('#videos')
+
+function clickinicio() {
+    inicio = document.querySelector('#inicio')
+    videos = document.querySelector('#videos')
+    login = document.querySelector('#login')
+
+    videos.style = "display:none; transition:500ms";
+
+    inicio.style = "display:flex; transition:500ms";
+}
+
+function clickvideos() {
+    inicio = document.querySelector('#inicio')
+    videos = document.querySelector('#videos')
+    login = document.querySelector('#login')
+
+    inicio.style = "display:none; transition:500ms";
+    login.style = "display:none; transition:500ms";
+
+    videos.style = "display:flex; transition:500ms";
+}
+
+function clickentrar() {
+    inicio = document.querySelector('#inicio')
+    videos = document.querySelector('#videos')
+    login = document.querySelector('#login')
+
+    inicio.style = "display:none; transition:500ms";
+    videos.style = "display:none; transition:500ms";
+
+    login.style = "display:flex; transition:500ms";
+}
+
+// Não arrastar o logo
+
+document.getElementById('#logo').ondragstart = function() { return false; };
